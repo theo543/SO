8b5f2e76475d4e1166b78c570d470138ba87517c
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-10-11 09:39:16 +0300

    spaces

--- Lab2/mycopy.c
+++ Lab2/mycopy.c
@@ -11,23 +11,28 @@ int main(int argc, char** args) {
         write(2, err, sizeof(err));
         return 1;
     }
+
     const char *src = args[1];
     const char *dst = args[2];
+
     struct stat st;
     if(stat(src, &st)) {
         perror("stat source file");
         return errno;
     }
+
     int srcfd = open(src, O_RDONLY);
     if(srcfd == -1) {
         perror("open source file");
         return errno;
     }
+
     char *buffer = malloc(st.st_size);
     if(buffer == NULL) {
         perror("allocate");
         return errno;
     }
+
     size_t bytesread = 0, byteswritten = 0;
     while(bytesread < st.st_size) {
         int ret = read(srcfd, buffer + bytesread, st.st_size - bytesread);
@@ -37,15 +42,18 @@ int main(int argc, char** args) {
         }
         bytesread += ret;
     }
+
     if(close(srcfd)) {
         perror("close source");
         return errno;
     }
+
     int dstfd = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0666);
     if(dstfd == -1) {
         perror("open destination file");
         return errno;
     }
+
     while(byteswritten < st.st_size) {
         int ret = write(dstfd, buffer + byteswritten, st.st_size - byteswritten);
         if(ret == -1) {
@@ -54,10 +62,13 @@ int main(int argc, char** args) {
         }
         byteswritten += ret;
     }
+
     free(buffer);
+
     if(close(dstfd)) {
         perror("close destination");
         return errno;
     }
+
     return 0;
 }
