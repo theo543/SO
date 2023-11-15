427b015bc01a3f5de8eb82063e5b6a70a3b1acbe
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-15 17:07:17 +0200

    Finish matmult implementation

--- Lab6/matmult.c
+++ Lab6/matmult.c
@@ -12,6 +12,7 @@
 sem_t threads_finished;
 sem_t threads_running;
 sem_t threads_running_or_idle;
+sem_t report_overflow_and_exit;
 
 void sem_init_noerr(sem_t *sem, int value) {
     if(sem_init(sem, 0, value) < 0) {
@@ -66,12 +67,17 @@ typedef struct matrix {
     i64 columns;
 } matrix_t;
 
-// No need to ever free the data in these.
-// Only 3 will ever be allocated.
-// The OS will free them on exit.
 matrix_t a, b, out;
 
-#define MATELEM(MATRIX, ROW, COLUMN) ((MATRIX).data[ROW * ((MATRIX).rows) + COLUMN])
+#define MATELEM(MATRIX, ROW, COLUMN) ((MATRIX).data[ROW * ((MATRIX).columns) + COLUMN])
+
+void free_mats_atexit() {
+    // These don't actually need to be freed, since they live until the end of the program,
+    // the OS would free them. But that causes noise when using valgrind.
+    if(a.data) free(a.data);
+    if(b.data) free(b.data);
+    if(out.data) free(out.data);
+}
 
 void alloc_mat(matrix_t *mat) {
     i64 alloc_size;
@@ -86,15 +92,15 @@ void alloc_mat(matrix_t *mat) {
 }
 
 void read_matrix(matrix_t *m) {
-    if(scanf(PRIi64 " " PRIi64, &m->rows, &m->columns) == EOF) {
-        perror("scanf");
+    if(scanf("%"PRIi64"%"PRIi64, &m->rows, &m->columns) != 2) {
+        fprintf(stderr, "Invalid matrix size.");
         exit(EXIT_FAILURE);
     }
     alloc_mat(m);
     for(i64 row = 0;row < m->rows;row++) {
         for(i64 col = 0;col < m->columns;col++) {
-            if(scanf(PRIi64, &MATELEM(*m, row, col)) == EOF) {
-                perror("scanf");
+            if(scanf("%"PRIi64, &MATELEM(*m, row, col)) != 1) {
+                fprintf(stderr, "Invalid matrix element at position (%"PRIi64", %"PRIi64")\n", row, col);
                 exit(EXIT_FAILURE);
             }
         }
@@ -107,18 +113,35 @@ typedef struct matmult_thread_args {
 } matmult_args_t;
 
 void * matmult_thread(void *args_void) {
-    sem_wait(&threads_running);
+    sem_wait_noerr(&threads_running);
     matmult_args_t *args = args_void;
-    ///TODO implement multiply
-    sem_post(&threads_running);
-    sem_post(&threads_running_or_idle);
-    sem_post(&threads_finished);
+    MATELEM(out, args->out_row, args->out_col) = args->out_row * out.columns + args->out_col;
+    i64 i = args->out_row;
+    i64 j = args->out_col;
+    i64 sum = 0;
+    for(i64 k = 0;k < a.columns;k++) {
+        i64 result;
+        if(multiply(MATELEM(a, i, k), MATELEM(b, k, j), &result)) {
+            sem_wait_noerr(&report_overflow_and_exit);
+            fprintf(stderr, "Overflow calculating result at position %"PRIi64"x%"PRIi64"\n", i, j);
+            exit(EXIT_FAILURE);
+        }
+        sum += result;
+    }
+    MATELEM(out, i, j) = sum;
+    free(args);
+    sem_post_noerr(&threads_running);
+    sem_post_noerr(&threads_running_or_idle);
+    sem_post_noerr(&threads_finished);
+    return NULL;
 }
 
 int main() {
+    atexit(free_mats_atexit);
     sem_init_noerr(&threads_finished, 0);
     sem_init_noerr(&threads_running, RUNNING_THREADS_ALLOWED);
     sem_init_noerr(&threads_running_or_idle, RUNNING_OR_IDLE_THREADS_ALLOWED);
+    sem_init_noerr(&report_overflow_and_exit, 1);
     read_matrix(&a);
     read_matrix(&b);
     if(a.columns != b.rows) {
@@ -128,6 +151,15 @@ int main() {
     out.rows = a.rows;
     out.columns = b.columns;
     alloc_mat(&out);
+    pthread_attr_t detached;
+    if(pthread_attr_init(&detached) != 0) {
+        perror("pthread_attr_init");
+        return errno;
+    }
+    if(pthread_attr_setdetachstate(&detached, PTHREAD_CREATE_DETACHED) != 0) {
+        perror("pthread_attr_setdetachstate");
+        return errno;
+    }
     for(i64 row = 0;row < out.rows;row++) {
         for(i64 col = 0;col < out.columns;col++) {
             sem_wait_noerr(&threads_running_or_idle);
@@ -135,20 +167,17 @@ int main() {
             matmult_args_t *args = malloc(sizeof(matmult_args_t));
             args->out_row = row;
             args->out_col = col;
-            if(pthread_create(&thr, NULL, matmult_thread, args)) {
+            if(pthread_create(&thr, &detached, matmult_thread, args)) {
                 perror("pthread_create");
-                return errno;
-            }
-            if(pthread_detach(&thr)) {
-                perror("pthread_detach");
                 return errno;
             }
         }
     }
+    pthread_attr_destroy(&detached);
     for(i64 x = 0;x < out.rows * out.columns;x++) {
-        sem_post(&threads_finished);
+        sem_wait_noerr(&threads_finished);
     }
-    printf("%"PRIi64"x%"PRIi64" output matrix:\n", out.rows, out.columns);
+    printf("%"PRIi64" %"PRIi64"\n", out.rows, out.columns);
     for(i64 row = 0;row < out.rows;row++) {
         for(i64 col = 0;col < out.columns;col++) {
             printf("%"PRIi64" ", MATELEM(out, row, col));
