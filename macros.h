e12ee6190855741f3821470010176d1b209081be
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-18 20:01:57 +0200

    Add file and line details to perror macro

--- macros.h
+++ macros.h
@@ -1,10 +1,19 @@
+#include <stdlib.h>
+
+#define PERROR_WITH_DETAILS(CALL)\
+do {\
+    /* SOURCE_PATH_SIZE defined in CMake */\
+    fprintf(stderr, "Error at file %s:%d => ", __FILE__ + SOURCE_PATH_SIZE, __LINE__);\
+    perror(#CALL);\
+} while (0)
+
 // Call pthread function, report any errors.
 #define PT_CALL(CALL)\
 do {\
     int result = CALL;\
     if(result != 0) {\
         errno = result;\
-        perror(#CALL);\
+        PERROR_WITH_DETAILS(CALL);\
         exit(errno);\
     }\
 } while (0)
@@ -14,7 +23,7 @@ do {\
 do {\
     int result = CALL;\
     if(result != 0) {\
-        perror(#CALL);\
+        PERROR_WITH_DETAILS(CALL);\
         exit(errno);\
     }\
 } while (0)
